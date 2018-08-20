08f9c22a2b010b393a5042398a6aaee5033a734d
Su Mon Yee <sumonyee@op-j.jp>
2018-08-20 15:19:43 +0630

    commit db for heroku
    commit db for heroku

--- config/database.php
+++ config/database.php
@@ -73,18 +73,18 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'schema' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
@@ -98,7 +98,7 @@ return [
         ],
 
         /***** Only for heroku *****/
-        'pg-heroku' => [
+        'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $heroku_db_url['host'],
             'database' => substr($heroku_db_url['path'], 1),
